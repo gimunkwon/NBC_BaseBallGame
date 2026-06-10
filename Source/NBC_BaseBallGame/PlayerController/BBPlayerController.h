9c9be2386e53f331200a3ef75761d417e22ac286
gimunkwon <gimon71@gmail.com>
2026-06-10 17:11:25 +0900

    [Add] PlayerController 클래스 추가

---
+++ Source/NBC_BaseBallGame/PlayerController/BBPlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BBPlayerController.generated.h"
+
+
+UCLASS()
+class NBC_BASEBALLGAME_API ABBPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
