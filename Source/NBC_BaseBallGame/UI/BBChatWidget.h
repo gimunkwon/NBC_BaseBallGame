2b79312aebc07fc23e878adbc61f0c7a3fb0ec48
gimunkwon <gimon71@gmail.com>
2026-06-10 17:58:56 +0900

    [Add] UMG 모듈추가 채팅UI 클래스 추가

---
+++ Source/NBC_BaseBallGame/UI/BBChatWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BBChatWidget.generated.h"
+
+UCLASS()
+class NBC_BASEBALLGAME_API UBBChatWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
