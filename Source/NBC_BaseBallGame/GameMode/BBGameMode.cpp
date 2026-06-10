382de8cde6cb90737ce07c41d3ec1fe3698b9a64
gimunkwon <gimon71@gmail.com>
2026-06-10 16:24:12 +0900

    [Add] Gamemode클래스 추가

---
+++ Source/NBC_BaseBallGame/GameMode/BBGameMode.cpp
@@ -0,0 +1,1 @@
+#include "BBGameMode.h"
---
+++ Source/NBC_BaseBallGame/GameMode/BBGameMode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BBGameMode.generated.h"
+
+UCLASS()
+class NBC_BASEBALLGAME_API ABBGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
