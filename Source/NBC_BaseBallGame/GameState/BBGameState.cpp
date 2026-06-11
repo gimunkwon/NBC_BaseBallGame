6d7f1a4624b5bb9c673da9ab2aedd9ac3bb5bfdc
gimunkwon <gimon71@gmail.com>
2026-06-11 17:45:52 +0900

    [Add] Gamestate클래스에 현재 게임의 진행상황, 잔여 시도 횟수 멤버변수 관리 및 레플리케이션 등록

--- Source/NBC_BaseBallGame/GameState/BBGameState.cpp
+++ Source/NBC_BaseBallGame/GameState/BBGameState.cpp
@@ -1,1 +1,18 @@
 #include "BBGameState.h"
+
+#include "Net/UnrealNetwork.h"
+
+ABBGameState::ABBGameState()
+	: RemainingAttempts(10)
+	, GamePhase(EBBGamePhase::Waiting)
+{
+	
+}
+
+void ABBGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(ThisClass, RemainingAttempts);
+	DOREPLIFETIME(ThisClass, GamePhase);
+}
--- Source/NBC_BaseBallGame/GameState/BBGameState.h
+++ Source/NBC_BaseBallGame/GameState/BBGameState.h
@@ -4,8 +4,31 @@
 #include "GameFramework/GameState.h"
 #include "BBGameState.generated.h"
 
+UENUM(BlueprintType)
+enum class EBBGamePhase: uint8
+{
+	Waiting,	// 게임 시작전
+	Playing,	// 게임 플레이 중
+	Win,		// 게임 승리
+	Lose		// 게임 패배
+};
+
 UCLASS()
 class NBC_BASEBALLGAME_API ABBGameState : public AGameState
 {
 	GENERATED_BODY()
+	
+public:
+	ABBGameState();
+	
+	// 남은 추측 횟수
+	UPROPERTY(Replicated, BlueprintReadOnly)
+	int32 RemainingAttempts;
+	// 현재 게임 진행 상황
+	UPROPERTY(Replicated,BlueprintReadOnly)
+	EBBGamePhase GamePhase;
+	
+#pragma region Override
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+#pragma endregion 
 };
