8bb2ee560d5b4e222017b2d5d6d6906437a82b3a
gimunkwon <gimon71@gmail.com>
2026-06-11 16:22:20 +0900

    [Add,Fix] 기존 위젯 인스턴스 생성 BeginPlay->PostNetInit으로 변경 로깅 로직 제거 -> 위젯 연동 로깅 로직으로 변경

--- Source/NBC_BaseBallGame/PlayerController/BBPlayerController.cpp
+++ Source/NBC_BaseBallGame/PlayerController/BBPlayerController.cpp
@@ -4,23 +4,44 @@
 #include "Kismet/KismetSystemLibrary.h"
 #include "NBC_BaseBallGame/UI/BBChatWidget.h"
 
-void ABBPlayerController::BeginPlay()
+// void ABBPlayerController::BeginPlay()
+// {
+// 	Super::BeginPlay();
+// }
+
+ABBPlayerController::ABBPlayerController()
 {
-	Super::BeginPlay();
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+}
+
+void ABBPlayerController::PostNetInit()
+{
+	Super::PostNetInit();
 	
-	if (Widget_ChatClass)
+	UE_LOG(LogTemp,Warning,TEXT("PostNetInit called | IsLocal : %d | WidgetClass: %d"),IsLocalController(), Widget_ChatClass != nullptr);
+	
+	if (IsLocalController() && Widget_ChatClass)
 	{
 		Widget_ChatInst = CreateWidget<UBBChatWidget>(this, Widget_ChatClass);
 		if (Widget_ChatInst)
 		{
 			Widget_ChatInst->AddToViewport();
 		}
-		
 	}
 }
 
 void ABBPlayerController::ClientReceiveChat_Implementation(const FString& SenderName, const FString& Message)
 {
 	FString LastString = FString::Printf(TEXT("[%s] : %s"),*SenderName, *Message);
-	UKismetSystemLibrary::PrintString(this, LastString, true, true, FLinearColor::Green, 5.f);
+	// UKismetSystemLibrary::PrintString(this, LastString, true, true, FLinearColor::Green, 5.f);
+	
+	if (Widget_ChatInst)
+	{
+		Widget_ChatInst->AddMessage(LastString);
+	}
 }
+
+
+
+
--- Source/NBC_BaseBallGame/PlayerController/BBPlayerController.h
+++ Source/NBC_BaseBallGame/PlayerController/BBPlayerController.h
@@ -13,6 +13,8 @@ class NBC_BASEBALLGAME_API ABBPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	ABBPlayerController();
+	
 #pragma region ChatFunc
 	UFUNCTION(Client,Reliable)
 	void ClientReceiveChat(const FString& SenderName, const FString& Message);
@@ -20,12 +22,13 @@ public:
 	
 protected:
 #pragma region Override
-	virtual void BeginPlay() override;
+	// virtual void BeginPlay() override;
+	virtual void PostNetInit() override;
 #pragma endregion 
 	
 #pragma region Widget
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="UI")
-	TSubclassOf<UUserWidget> Widget_ChatClass;
+	TSubclassOf<UBBChatWidget> Widget_ChatClass;
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="UI|Instance")
 	TObjectPtr<UBBChatWidget> Widget_ChatInst;
 #pragma endregion 
