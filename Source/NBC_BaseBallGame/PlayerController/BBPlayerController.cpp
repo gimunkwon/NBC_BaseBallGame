bb9dcc79a7f21f80dcd89cc6888287dca12a3fb1
gimunkwon <gimon71@gmail.com>
2026-06-11 11:55:15 +0900

    [Add] 채팅 위젯 인스턴스 생성

--- Source/NBC_BaseBallGame/PlayerController/BBPlayerController.cpp
+++ Source/NBC_BaseBallGame/PlayerController/BBPlayerController.cpp
@@ -1,6 +1,23 @@
 #include "BBPlayerController.h"
 
+#include "Blueprint/UserWidget.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "NBC_BaseBallGame/UI/BBChatWidget.h"
+
+void ABBPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (Widget_ChatClass)
+	{
+		Widget_ChatInst = CreateWidget<UBBChatWidget>(this, Widget_ChatClass);
+		if (Widget_ChatInst)
+		{
+			Widget_ChatInst->AddToViewport();
+		}
+		
+	}
+}
 
 void ABBPlayerController::ClientReceiveChat_Implementation(const FString& SenderName, const FString& Message)
 {
--- Source/NBC_BaseBallGame/PlayerController/BBPlayerController.h
+++ Source/NBC_BaseBallGame/PlayerController/BBPlayerController.h
@@ -5,14 +5,28 @@
 #include "BBPlayerController.generated.h"
 
 
+class UBBChatWidget;
+
 UCLASS()
 class NBC_BASEBALLGAME_API ABBPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
 public:
-	//TODO::ClientRPC 함수
-	// 즉 대화를 출력할 메서드
+#pragma region ChatFunc
 	UFUNCTION(Client,Reliable)
 	void ClientReceiveChat(const FString& SenderName, const FString& Message);
+#pragma endregion 
+	
+protected:
+#pragma region Override
+	virtual void BeginPlay() override;
+#pragma endregion 
+	
+#pragma region Widget
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="UI")
+	TSubclassOf<UUserWidget> Widget_ChatClass;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="UI|Instance")
+	TObjectPtr<UBBChatWidget> Widget_ChatInst;
+#pragma endregion 
 };
