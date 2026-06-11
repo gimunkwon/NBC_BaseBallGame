15402962ec93480606a48e2523d06811d976e39c
gimunkwon <gimon71@gmail.com>
2026-06-11 17:31:16 +0900

    [Add] GameState 클래스 추가

---
+++ Source/NBC_BaseBallGame/GameState/BBGameState.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "BBGameState.generated.h"
+
+UCLASS()
+class NBC_BASEBALLGAME_API ABBGameState : public AGameState
+{
+	GENERATED_BODY()
+};
