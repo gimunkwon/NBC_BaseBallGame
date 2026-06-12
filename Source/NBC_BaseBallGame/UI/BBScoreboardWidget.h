41891d47630a210297849560dd3ecded9d899cfb
gimunkwon <gimon71@gmail.com>
2026-06-12 19:14:57 +0900

    [Add] ScoreBoard 위젯 클래스 추가

---
+++ Source/NBC_BaseBallGame/UI/BBScoreboardWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BBScoreboardWidget.generated.h"
+
+UCLASS()
+class NBC_BASEBALLGAME_API UBBScoreboardWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
