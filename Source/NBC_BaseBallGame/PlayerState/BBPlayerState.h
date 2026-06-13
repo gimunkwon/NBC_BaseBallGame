030d6c115548891ceced65974795ab9e517c8fe6
gimunkwon <gimon71@gmail.com>
2026-06-13 19:52:29 +0900

    [Add] 플레이어의 준비 상태에 따른 게임시작을위한 레플리케이션 플래그 변수

--- Source/NBC_BaseBallGame/PlayerState/BBPlayerState.h
+++ Source/NBC_BaseBallGame/PlayerState/BBPlayerState.h
@@ -8,4 +8,10 @@ UCLASS()
 class NBC_BASEBALLGAME_API ABBPlayerState : public APlayerState
 {
 	GENERATED_BODY()
+	
+public:
+	UPROPERTY(Replicated, BlueprintReadOnly)
+	bool bIsReady = false;
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 };
